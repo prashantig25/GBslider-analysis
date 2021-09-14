3610e4a2840d603e3ca6141bd0b395ffbe6e9d14
prashantig25 <75947365+prashantig25@users.noreply.github.com>
2021-09-14 11:11:59 +0200

    Update simulations.m

--- simulations.m
+++ simulations.m
@@ -1,21 +1,22 @@
 function [df] = simulations
+    % This function runs simulation of different agents.
     
-    % parameters from the taskvars class
-    task_vars = taskvars()
+    
+    % task and agent parameters required for the simulations
+    task_vars = taskvars();;
     agent_vars = agentvars()
     
     % number of trials and blocks
     n_trials = task_vars.T
     n_blocks = task_vars.B
     
-    df_sims = DataFrame();
-    
+    % Cycle over number of blocks
     for b = 1:n_blocks
         
         % Single block task-agent-interaction simulation
         data_sims = task_agent_int();
-%         df_sims.data_sims = data_sims;
-%         df = df_sims
+        
+        % save dataframes in an excel sheet for different blocks
         data_xls = sprintf('simulations.xlsx');
         writetable(data_sims,data_xls,'sheet',b);
     end 
