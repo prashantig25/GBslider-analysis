9848d41e474741189453d14aa33031f7f0d8ad4b
Prashanti Ganesh <prashantig25@gmail.com>
2023-10-11 10:35:14 +0000

    script to fit linear regression model

--- linear_fit.m
+++ linear_fit.m
@@ -1,4 +1,5 @@
-function [betas,rsquared,residuals] = linear_fit(tbl,mdl,pred_vars,resp_var,cat_vars,num_vars,weight_y_n)
+function [betas,rsquared,residuals,coeffs_name,lm] = linear_fit(tbl,mdl,pred_vars,resp_var, ...
+    cat_vars,num_vars,weight_y_n,varargin)
 
     % LINEAR_FIT fits a linear regression model to the updates as a
     % function of prediction error and other task based computational
@@ -18,6 +19,8 @@ function [betas,rsquared,residuals] = linear_fit(tbl,mdl,pred_vars,resp_var,cat_
         % betas = array containing beta value for each predictor by fitlm
         % rsquared = rsquared after fitting mdl to the data
         % residuals  = residuals after fitting mdl to the data
+        % coeffs_name = cell array containing name of all regressors
+        % lm = fitted model
     
     % FIT THE MODEL USING WEIGHTED/NON-WEIGHTED REGRESSION
     if weight_y_n == 1
@@ -35,4 +38,5 @@ function [betas,rsquared,residuals] = linear_fit(tbl,mdl,pred_vars,resp_var,cat_
     for b = 1:num_vars+1
         betas(1,b) = lm.Coefficients.Estimate(b);
     end
+    coeffs_name = lm.CoefficientNames;
 end
