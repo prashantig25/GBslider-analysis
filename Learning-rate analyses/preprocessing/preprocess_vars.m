62d10bee3bf0f48a0af0381b368d0f24ccfcfe13
Prashanti Ganesh <prashantig25@gmail.com>
2024-03-06 07:01:06 +0000

    preprocessing for LR analysis

---
+++ Learning-rate analyses/preprocessing/preprocess_vars.m
@@ -0,0 +1,22 @@
+classdef preprocess_vars < handle
+% PREPROCESS_VARS is a superclass that specifies variables for the preprocessing of
+% behavioural data, to compute regressors for model based analyses.
+    properties
+        filename = 'study2.xlsx'; % path of file with behavioural data
+        agent = 0 % if analysis is being run for normative agent
+        pupil = 0; % if analysis is being run for behavioural data from pupil study
+        removed_cond = 3 % experimental condition number that is to be excluded during analysis
+        num_subjs = 99; % number of participants
+        absolute_lr = 0; % if analysis should be fit to absolute UPs and absolute PEs 
+        data % table with behavioural data
+        mu % reported contingency parameter/reward probability
+        flipped_mu % reported contingency parameter/reward probability, corrected for congruence ref. eq 16
+        obtained_reward % task generated reward
+        condition % experimental condition
+        action % participant's action
+        state % trial state
+        recoded_reward % reward recoded contingent on a (recoded to a = 0)
+        mu_t % estimated mu for current trial
+        mu_t_1 % estimated mu for previous trial
+    end
+end
