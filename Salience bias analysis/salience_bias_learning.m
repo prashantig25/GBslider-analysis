07799b908450806082bc883d430f3a25be82fe53
Prashanti Ganesh <prashantig25@gmail.com>
2024-03-06 07:41:31 +0000

    salience bias in DM and learning

---
+++ Salience bias analysis/salience_bias_learning.m
@@ -0,0 +1,30 @@
+clc
+clearvars
+
+% INITIALISE
+id_subjs = unique(data_subjs.id); % subject IDs
+mdl = 'up ~ pe + pe:contrast_diff + pe:salience_choice + pe:congruence + pe:pe_sign'; % model definition
+pred_vars = {'pe','salience','contrast_diff','congruence','condition','reward_unc','subj_est_unc' ...
+        ,'reward','mu','pe_sign','pu','salience_choice'}; % cell array with names of predictor variables
+resp_var = 'up'; % name of response variable
+cat_vars = {'salience','congruence','condition','reward_unc','pe_sign','salience_choice'}; % cell array with names of categorical variables
+num_vars = 5; % number of predictor vars
+res_subjs = []; % empty array to store residuals
+weight_y_n = 0; % non-weighted regression
+num_subjs = 1:length(id_subjs); % number of subjects
+
+% FIT MODEL
+salience_bias = lr_analysis_obj(); % linear regression object
+salience_bias.filename = 'preprocessed_subj.xlsx'; % use the estimation error data file
+salience_bias.mdl = mdl; % model to be fit
+salience_bias.pred_vars = pred_vars; % predictor variables
+salience_bias.resp_var = resp_var; % response variable
+salience_bias.cat_vars = cat_vars; % categorical variables
+salience_bias.num_vars = num_vars; % number of regressors
+salience_bias.weighted = 0; % non-weighted regression
+salience_bias.weight_y_n = 0;
+salience_bias.absolute_analysis = 1; % absolute analysis or not
+
+[betas_all,rsquared,~,coeffs_name,h_vals,p_vals,t_vals,posterior_up_subjs] = salience_bias.get_coeffs(data_subjs,mdl,pred_vars, ...
+    resp_var,cat_vars,num_vars,res_subjs,num_subjs,1);
+save("betas_abs_salience.mat","betas_all") % save file
---
+++ Salience bias analysis/saliencebias_analysis.m
@@ -0,0 +1,56 @@
+clc
+clearvars
+
+% PREPARE DATA
+data = readtable("study1.xlsx"); % load
+all_cond = 1; % whether all conditions to be considered for regression
+if all_cond == 0
+    data(data.choice_cond == 3,:) = []; % remove condition, if neccesary
+end
+
+% CATEGORICALLY CODE PERCEPTUAL AND REWARD UNCERTAINTY
+for i = 1:height(data) 
+    if data.condition_int(i) == 1 || data.condition_int(i) == 3
+        data.reward_unc(i) = 1; % reward uncertainty
+    else
+        data.reward_unc(i) = 0;
+    end
+    if data.condition_int(i) == 4 || data.condition_int(i) == 3
+        data.pu(i) = 0; % perceptual uncertainty
+    else
+        data.pu(i) = 1;
+    end
+end
+
+% FIT MODEL
+num_blocks = 16; % number of blocks
+subj_id = unique(data.id); % subject IDs
+mdl = 'ecoperf ~ pu + reward_unc + contrast'; % model
+num_vars = 3; % number of variables
+block_data = NaN(num_blocks,num_vars); % store block wise task variables and economic performance
+pred_vars = {'contrast','pu','reward_unc'}; % cell array with names of predictor variables
+resp_var = 'ecoperf'; % name of response variable
+cat_vars = {'contrast','pu','reward_unc'}; % cell array with names of categorical variables
+num_vars = 3; % number of predictor vars
+res_subjs = []; % empty array to store residuals
+weight_y_n = 0; % non-weighted regression
+betas_all = NaN(length(subj_id),num_vars);
+
+for i = 1:length(subj_id)
+    data_subj = data(data.id == subj_id(i),:);
+    for b = 1:num_blocks % get task-based variables and economic performance for each block
+        data_blocks = data_subj(data_subj.blocks == b,:);
+        block_data(b,1) = unique(data_blocks.reward_unc);
+        block_data(b,2) = unique(data_blocks.pu);
+        block_data(b,3) = unique(data_blocks.contrast);
+        block_data(b,4) = nanmean(data_blocks.ecoperf);
+    end
+    tbl = table(block_data(:,1),block_data(:,2),block_data(:,3),block_data(:,4), ...
+        'VariableNames',{'reward_unc','pu','contrast','ecoperf'});
+    [betas,rsquared,residuals,coeffs_name,lm] = linear_fit(tbl,mdl,pred_vars,resp_var, ...
+        cat_vars,num_vars,weight_y_n);
+    betas_all(i,:) = betas(2:end);
+end
+
+% SAVE DATA
+save("betas_salience_study1","betas_all")
