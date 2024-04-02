dadfe082beb2f40f2e925463ba40b1a1f967c568
Prashanti Ganesh <prashantig25@gmail.com>
2024-04-02 11:45:06 +0000

    updated function documentation style

--- Stats/compute_cohend_ttest2.m
+++ Stats/compute_cohend_ttest2.m
@@ -1,11 +1,13 @@
 function cohen_d = compute_cohend_ttest2(mean1,mean2,sd_pooled)
-    % compute_cohend_ttest2 computes cohen's d value for paired t-test.
+    %
+    % function compute_cohend_ttest2 computes cohen's d value for paired t-test.
     % INPUTS:
-        % mean1: mean of within-subjects group 1
-        % mean2: mean of within-subjects group 2
-        % sd_pooled: pooled SD
-
+    % mean1: mean of within-subjects group 1
+    % mean2: mean of within-subjects group 2
+    % sd_pooled: pooled SD
+    %
     % OUTPUT:
-        % cohen_d: cohen's d
+    % cohen_d: cohen's d
+    %
     cohen_d = (mean2-mean1)./sd_pooled;
 end
