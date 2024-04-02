3cfb985d6e2868eda1d2f2543c38870867c6aeeb
Prashanti Ganesh <prashantig25@gmail.com>
2024-04-02 09:43:40 +0000

    updated function documentation style

--- Figures/utilities/adjust_figprops.m
+++ Figures/utilities/adjust_figprops.m
@@ -1,19 +1,19 @@
 function adjust_figprops(axes,font_name,font_size,line_width,varargin)
-    
-    % adjust_figprops adjusts various figure or tile properties.
-    
+    %
+    % function adjust_figprops adjusts various figure or tile properties.
+    %
     % INPUTS:
-        % axes: current axes
-        % font_name: font name
-        % font_size: font size
-        % line_width: line width
-        % varargin{1}: limit for x-axis
-        % varargin{2}: limit for y-axis
-      
+    % axes: current axes
+    % font_name: font name
+    % font_size: font size
+    % line_width: line width
+    % varargin{1}: limit for x-axis
+    % varargin{2}: limit for y-axis
+    %
     set(axes,'Color','none','FontName',font_name,'FontSize',font_size)
     set(axes,'LineWidth',line_width)
     if ~isempty(varargin)
-        xlim(varargin{1})
-        ylim(varargin{2})
+        xlim(axes,varargin{1})
+        ylim(axes,varargin{2})
     end
 end
