65f64fdd141d86d2b0a4af59a64eceeb69595d4d
Prashanti Ganesh <prashantig25@gmail.com>
2024-04-03 09:43:19 +0000

    added linebreak, indent in function documentation

--- Stats/compute_partialrsq.m
+++ Stats/compute_partialrsq.m
@@ -1,15 +1,15 @@
 function [partial_rsq] = compute_partialrsq(rsq_reduced,rsq_full)
-    %
+    
     % function compute_partialrsq computes partial r-squared value for a single
     % coefficient from a multi-variate model.
     %
     % INPUTS:
-    % rsq_reduced: R-squared of the reduced model (model excluding the term
-    % that needs to be evaluated).
-    % rsq_full: R-squared of the full model
+    %   rsq_reduced: R-squared of the reduced model (model excluding the term
+    %   that needs to be evaluated).
+    %   rsq_full: R-squared of the full model
     %
     % OUTPUT:
-    % partial_rsq: partial r-squared
-    %
+    %   partial_rsq: partial r-squared
+    
     partial_rsq = (rsq_full - rsq_reduced)./rsq_full;
 end
