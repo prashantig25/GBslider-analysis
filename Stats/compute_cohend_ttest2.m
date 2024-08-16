192517ad6078502d8cd641e8e667c659455c65f1
Prashanti Ganesh <prashantig25@gmail.com>
2024-08-16 14:37:15 +0000

    updated cohen's function to compute for both one-tailed and paired ttests

--- Stats/compute_cohend_ttest2.m
+++ Stats/compute_cohend_ttest2.m
@@ -1,11 +1,14 @@
 function cohen_d = compute_cohend_ttest2(mean1,mean2,sd_pooled)
     
-    % function compute_cohend_ttest2 computes cohen's d value for paired t-test.
+    % function compute_cohend_ttest2 computes cohen's d value for 
+    % one tailed and paired t-test.
     % INPUTS:
-    %   mean1: mean of within-subjects group 1
-    %   mean2: mean of within-subjects group 2
-    %   sd_pooled: pooled SD
-    %
+    %   mean1: for two-tailed, mean of within-subjects group 1
+    %          for one-tailed, sample mean
+    %   mean2: for two-tailed, mean of within-subjects group 2
+    %          for one-tailed, population mean
+    %   sd_pooled: for two-tailed, pooled SD
+    %              for one-tailed, sample DF
     % OUTPUT:
     %   cohen_d: cohen's d
     
