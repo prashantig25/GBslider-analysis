06bd749e18b9857ab373153fe74f73d74c137464
prashantig25 <75947365+prashantig25@users.noreply.github.com>
2024-10-31 14:02:16 +0100

    updated script to run all analyses automatically

--- Estimation error analysis/esterror_analysis.m
+++ Estimation error analysis/esterror_analysis.m
@@ -1,114 +1,201 @@
+% esterror_analysis computes the relationship between fixed and flexible
+% LRs with the participants' belief accuracy operationalized as the mean
+% estimation error
+
 clc
 clearvars
 
-% Get the current working directory
-currentDir = pwd;
-save_dir = strcat('Data', filesep, 'estimation error analysis'); 
+% PATH 
+
+currentDir = cd;
+reqPath = 'Reward-learning-analysis (code_review)'; % to which directory one must save in
+pathParts = strsplit(currentDir, filesep);
+if strcmp(pathParts{end}, reqPath)
+    disp('Current directory is already the desired path. No need to run createSavePaths.');
+    desiredPath = currentDir;
+else
+    % Call the function to create the desired path
+    desiredPath = createSavePaths(currentDir, reqPath);
+end
+save_dir = strcat(desiredPath, filesep, 'Data', filesep, 'estimation error analysis'); 
 mkdir(save_dir);
 
-% LOAD DATA
-abs_lr = 1; % if absolute LRs are to be used for estimation error analysis
-if abs_lr == 1
-    betas_all = importdata(fullfile('Data',filesep,'LR analyses' ...
-        ,filesep,'betas_abs_wo_rewunc_obj.mat')); % betas from lr analysis model
-else
-    betas_all = importdata(fullfile('Data',filesep,'LR analyses' ...
-        ,filesep,'betas_signed_wo_rewunc_obj.mat')); % betas from lr analysis model
-end
-data = readtable(fullfile('Data',filesep,'LR analyses',filesep,'preprocessed_data.xlsx')); % get choice performance
+% INITIALIZE VARS
 
-% INITIALISE VARS
 num_subjs = 98; % number of subjects
-ecoperf = NaN(num_subjs,1); % initialised array for economic performance
-esterror = NaN(num_subjs,1); % initialised array for estimation error
-id_subjs = unique(data.ID); % subject IDs
-save_mat = 1; % set to 1, if data needs to be saved for plotting
-abs_esterror = 1; % set to 1, if analysis needs to be done for absolute estimation error
-num_vars = 5; % number of variables
-num_vars_partial = 4; % number of variables for partial R2
-pred_vars = {'pe','pe__condiff','pe__salience','pe__congruence','pe__rewunc','pe__pesign'}; % predictor variables
-resp_var = 'perf'; % response variable
-cat_vars = ''; % categorical variables
-res_subjs = []; % residuals
-
-% INITIALISE MODELS
 mdl = 'perf ~ pe + pe__condiff  + pe__pesign + pe__salience + pe__congruence'; % full model
 mdl_pe = 'perf ~ pe__condiff + pe__salience + pe__congruence + pe__pesign'; % partial model without fixed LR
 mdl_pe_condiff = 'perf ~ pe + pe__salience + pe__congruence + pe__pesign'; % partial model without BS adapted LR
 mdl_pe_pesign = 'perf ~ pe + pe__condiff + pe__salience + pe__congruence'; % partial model without confirmation bias
 mdl_pe_salience = 'perf ~ pe + pe__condiff + pe__congruence + pe__pesign'; % partial model without salience
 mdl_pe_congruence = 'perf ~ pe + pe__condiff + pe__salience + pe__pesign'; % partial model without congruence
-%% FIT LEARNING RATE ANALYSIS COEFFICIENTS TO MEAN PERFORMANCE
+var_names = {'pe','pe__condiff','pe__salience','pe__congruence','pe__pesign','perf'}; % variable names
+
+% LOAD DATA
+
+betas_all = importdata(fullfile(desiredPath, filesep,'Data',filesep,'LR analyses' ...
+        ,filesep,'betas_signed_wo_rewunc_obj.mat')); % betas from lr analysis model
+data = importdata(fullfile(desiredPath, filesep, 'Data',filesep,'LR analyses',filesep,'preprocessed_data.mat')); % get choice performance
+
 
 % COMPUTE ESTIMATION ERROR
+
+id_subjs = unique(data.ID); % subject IDs
 for i = 1:height(data)
     if data.choice_cond(i) == 2 % actual mu for each condition
         data.actual_mu(i) = 0.9; 
     else 
         data.actual_mu(i) = 0.7;
     end
-    data.est_error(i) = data.mu_congruence(i) - data.actual_mu(i); 
-    if abs_esterror == 1
-        data.est_error(i) = abs(data.est_error(i));
-    end
+    data.signed_est_error(i) = data.mu_congruence(i) - data.actual_mu(i); % signed
+    data.abs_est_error(i) = abs(data.signed_est_error(i)); % absolute
 end
-
-% GET ECONOMIC PERFORMANCE & EST. ERROR
 for i = 1:num_subjs
-    ecoperf(i,:) = nanmean(data.ecoperf(data.ID == id_subjs(i)));
-    esterror(i,:) = nanmean(data.est_error(data.ID == id_subjs(i)));
+    absEE(i,:) = nanmean(data.abs_est_error(data.ID == id_subjs(i)));
+    signedEE(i,:) = nanmean(data.signed_est_error(data.ID == id_subjs(i)));
 end
 
 % NORMALISE ALL COEFFICIENTS
+
 norm_betas_all = betas_all;
+num_vars = 5;
 for i = 1:num_vars
     norm_betas_all(:,i) = normalise_zero_one(betas_all(:,i),NaN(height(betas_all),1));
 end
-norm_ecoperf = normalise_zero_one(ecoperf,NaN(height(betas_all),1));
-norm_esterror = normalise_zero_one(esterror,NaN(height(betas_all),1));
-data = [norm_betas_all,norm_esterror];
+norm_absEE = normalise_zero_one(absEE,NaN(height(betas_all),1));
+norm_signedEE = normalise_zero_one(signedEE,NaN(height(betas_all),1));
 
-var_names = {'pe','pe__condiff','pe__salience','pe__congruence','pe__pesign','perf'}; % variable names
+data = [norm_betas_all,norm_absEE];
 data_tbl = array2table(data, 'VariableNames', var_names); % data table
-safe_saveall(fullfile(save_dir,"esterror_analysis_abs_error_abs_lr.xlsx"),data_tbl); % save table
+safe_saveall(fullfile(save_dir,"esterror_analysis_abs_error_signed_lr.mat"),data_tbl); % save table
 
-% INITIALISE VARIABLES TO FIT MODEL
+% FIT MODEL ABS EE and SIGNED LR (Figure 5 and S8)
+
 esterror = lr_analysis_obj(); % linear regression object
-esterror.filename = 'Data\estimation error analysis\esterror_analysis_abs_error_abs_lr.xlsx'; % use the estimation error data file
-esterror.mdl = mdl; % model to be fit
-esterror.pred_vars = pred_vars; % predictor variables
-esterror.resp_var = resp_var; % response variable
-esterror.cat_vars = cat_vars; % categorical variables
-esterror.num_vars = num_vars; % number of regressors
+esterror.filename = strcat(desiredPath, filesep, "Data", filesep, "estimation error analysis", filesep, "esterror_analysis_abs_error_signed_lr.mat"); % use the estimation error data fil
+esterror.initialiseVars; % initialise vars
+esterror.EEanalysis = 1; % fit model to conduct estimation error analysis
+esterror.model_definition(mdl); % define model
 esterror.weighted = 0; % non-weighted regression
 esterror.weight_y_n = 0;
 esterror.absolute_analysis = 0; % absolute analysis or not
 
 % FIT FULL AND PARTIAL MODELS
+
 [~,~,~,~,lm] = esterror.linear_fit(data_tbl,@fitlm);
-esterror.num_vars = num_vars_partial;
-esterror.mdl = mdl_pe;
+esterror.model_definition(mdl_pe);
 [~,~,~,~,lm_pe] = esterror.linear_fit(data_tbl,@fitlm);
-esterror.mdl = mdl_pe_condiff;
+esterror.model_definition(mdl_pe_condiff);
 [~,~,~,~,lm_pe_condiff] = esterror.linear_fit(data_tbl,@fitlm);
-esterror.mdl = mdl_pe_pesign;
+esterror.model_definition(mdl_pe_pesign);
 [~,~,~,~,lm_pe_pesign] = esterror.linear_fit(data_tbl,@fitlm);
-esterror.mdl = mdl_pe_salience;
+esterror.model_definition(mdl_pe_salience);
 [~,~,~,~,lm_pe_salience] = esterror.linear_fit(data_tbl,@fitlm);
-esterror.mdl = mdl_pe_congruence;
-[betas,rsquared,residuals,coeffs_name,lm_pe_congruence] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_congruence);
+[~,~,~,~,lm_pe_congruence] = esterror.linear_fit(data_tbl,@fitlm);
 
 % COMPUTE PARTIAL R-SQAURE
-rsq_full = lm.Rsquared.Ordinary; % r-square for full model
-rsq_partial = [lm_pe.Rsquared.Ordinary; lm_pe_condiff.Rsquared.Ordinary; lm_pe_salience.Rsquared.Ordinary; lm_pe_congruence.Rsquared.Ordinary
-    lm_pe_pesign.Rsquared.Ordinary]; % r-square for partial models
-partial_rsq = NaN(5,1);
+
+SSE_full = lm.SSE; % r-square for full model
+SSE_reduced = [lm_pe.SSE; lm_pe_condiff.SSE; lm_pe_salience.SSE; lm_pe_congruence.SSE
+    lm_pe_pesign.SSE]; % r-square for partial models
+partial_rsqSSE = NaN(5,1);
 for i = 1:num_vars
-    partial_rsq(i,1) = abs(compute_partialrsq(rsq_partial(i),rsq_full));
+    partial_rsqSSE(i,1) = compute_partialrsqSSE(SSE_reduced(i),SSE_full);
 end
 
 % SAVE DATA
-if save_mat == 1
-    safe_saveall(fullfile(save_dir,filesep,"lm_abs_esterror_abs_lr.mat"),lm);
-    safe_saveall(fullfile(save_dir,filesep,"partialrsq_abs_esterror_abs_lr.mat"),partial_rsq);
+
+safe_saveall(fullfile(save_dir,filesep,"lm_abs_esterror_signed_lr.mat"),lm);
+safe_saveall(fullfile(save_dir,filesep,"partialrsq_abs_esterror_signed_lr.mat"),partial_rsqSSE);
+
+% FIT MODEL TO SIGNED EE AND SIGNED LR (Figure S7)
+
+data = [norm_betas_all,norm_signedEE];
+data_tbl = array2table(data, 'VariableNames', var_names); % data table
+safe_saveall(fullfile(save_dir,filesep,"esterror_analysis_signed_both.mat"),data_tbl); % save table
+esterror.filename = fullfile(save_dir,filesep,"esterror_analysis_signed_both.mat"); % use the estimation error data file
+esterror.initialiseVars;
+esterror.model_definition(mdl);
+
+% FIT FULL AND PARTIAL MODELS
+
+[~,~,~,~,lm] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe);
+[~,~,~,~,lm_pe] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_condiff);
+[~,~,~,~,lm_pe_condiff] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_pesign);
+[~,~,~,~,lm_pe_pesign] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_salience);
+[~,~,~,~,lm_pe_salience] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_congruence);
+[~,~,~,~,lm_pe_congruence] = esterror.linear_fit(data_tbl,@fitlm);
+
+% COMPUTE PARTIAL R-SQAURE
+
+SSE_full = lm.SSE; % r-square for full model
+SSE_reduced = [lm_pe.SSE; lm_pe_condiff.SSE; lm_pe_salience.SSE; lm_pe_congruence.SSE
+    lm_pe_pesign.SSE]; % r-square for partial models
+partial_rsqSSE = NaN(5,1);
+for i = 1:num_vars
+    partial_rsqSSE(i,1) = compute_partialrsqSSE(SSE_reduced(i),SSE_full);
 end
+
+% SAVE DATA
+
+safe_saveall(fullfile(save_dir,filesep,"lm_signed_esterror_signed_lr.mat"),lm);
+safe_saveall(fullfile(save_dir,filesep,"partialrsq_signed_esterror_signed_lr.mat"),partial_rsqSSE);
+
+% LOAD DATA
+
+betas_all = importdata(fullfile(desiredPath, filesep, 'Data',filesep,'LR analyses' ...
+        ,filesep,'betas_abs_wo_rewunc_obj.mat')); % betas from lr analysis model
+norm_betas_all = betas_all;
+for i = 1:num_vars
+    norm_betas_all(:,i) = normalise_zero_one(betas_all(:,i),NaN(height(betas_all),1));
+end
+
+data = [norm_betas_all,norm_absEE];
+data_tbl = array2table(data, 'VariableNames', var_names); % data table
+safe_saveall(fullfile(save_dir,"esterror_analysis_abs_error_abs_lr.mat"),data_tbl); % save table
+
+% FIT MODEL TO ABS EE AND ABS LR (Figure S9)
+
+esterror = lr_analysis_obj(); % linear regression object
+esterror.filename = strcat(desiredPath, filesep, 'Data', filesep, 'estimation error analysis', filesep, 'esterror_analysis_abs_error_abs_lr.mat'); % use the estimation error data file
+esterror.initialiseVars;
+esterror.EEanalysis = 1;
+esterror.model_definition(mdl);
+esterror.weighted = 0; % non-weighted regression
+esterror.weight_y_n = 0;
+esterror.absolute_analysis = 0; % absolute analysis or not
+
+% FIT FULL AND PARTIAL MODELS
+
+[~,~,~,~,lm] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe);
+[~,~,~,~,lm_pe] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_condiff);
+[~,~,~,~,lm_pe_condiff] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_pesign);
+[~,~,~,~,lm_pe_pesign] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_salience);
+[~,~,~,~,lm_pe_salience] = esterror.linear_fit(data_tbl,@fitlm);
+esterror.model_definition(mdl_pe_congruence);
+[betas,rsquared,residuals,coeffs_name,lm_pe_congruence] = esterror.linear_fit(data_tbl,@fitlm);
+
+% COMPUTE PARTIAL R-SQAURE
+
+SSE_full = lm.SSE; % r-square for full model
+SSE_reduced = [lm_pe.SSE; lm_pe_condiff.SSE; lm_pe_salience.SSE; lm_pe_congruence.SSE
+    lm_pe_pesign.SSE]; % r-square for partial models
+partial_rsqSSE = NaN(5,1);
+for i = 1:num_vars
+    partial_rsqSSE(i,1) = compute_partialrsqSSE(SSE_reduced(i),SSE_full);
+end
+
+% SAVE DATA
+
+safe_saveall(fullfile(save_dir,filesep,"lm_abs_esterror_abs_lr.mat"),lm);
+safe_saveall(fullfile(save_dir,filesep,"partialrsq_abs_esterror_abs_lr.mat"),partial_rsqSSE);
